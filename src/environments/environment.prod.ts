f9d23504c796f16a596553b1477b039624eec9ca
spotify-hero-community@okdoc.ai <spotify-hero-community@okdoc.ai>
2026-04-03 04:55:39 +0400

    Auth switch to # strategy for github pages

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   spotifyClientId: 'db432a608fab477e9638b8c3d5fa8ac7',
-  redirectUri: 'https://okdoc-ai-community-untrusted.github.io/simple-spotify-hero-app/callback',
+  redirectUri: 'https://okdoc-ai-community-untrusted.github.io/simple-spotify-hero-app/#/callback',
 };
